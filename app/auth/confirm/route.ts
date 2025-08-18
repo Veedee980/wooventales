50977989bed99e59296db226dfcb70757f07f35e
Verna <vdumbatsuro2@gmail.com>
2025-08-18 10:16:03 +0200

    done

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -1,30 +1,30 @@
-import { createClient } from "@/lib/supabase/server";
+import { createServerSupabaseClient } from "@/lib/supabase/server";
 import { type EmailOtpType } from "@supabase/supabase-js";
 import { redirect } from "next/navigation";
 import { type NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
-  const token_hash = searchParams.get("token_hash");
+  const token = searchParams.get("token") ?? "";
   const type = searchParams.get("type") as EmailOtpType | null;
+  const email = searchParams.get("email") ?? "";
   const next = searchParams.get("next") ?? "/";
 
-  if (token_hash && type) {
-    const supabase = await createClient();
-
-    const { error } = await supabase.auth.verifyOtp({
-      type,
-      token_hash,
-    });
-    if (!error) {
-      // redirect user to specified redirect URL or root of app
-      redirect(next);
-    } else {
-      // redirect the user to an error page with some instructions
-      redirect(`/auth/error?error=${error?.message}`);
-    }
+  if (!token || !type || !email) {
+    redirect(`/auth/error?error=Missing token, type, or email`);
   }
 
-  // redirect the user to an error page with some instructions
-  redirect(`/auth/error?error=No token hash or type`);
+  const supabase = await createServerSupabaseClient();
+
+  const { error } = await supabase.auth.verifyOtp({
+    type,
+    token,
+    email, // must include email for these types
+  });
+
+  if (!error) {
+    redirect(next);
+  } else {
+    redirect(`/auth/error?error=${error.message}`);
+  }
 }
